24654853cf0d6ae1995851381eca419a55f0e092
Andrei Kislichenko <android.2net@gmail.com>
2024-12-16 23:18:44 -0500

    fixes #64

--- spec/relaton_bipm/committee_spec.rb
+++ spec/relaton_bipm/committee_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe RelatonBipm::Committee do
       RelatonBipm::Committee.new acronym: "INVAL"
     end.to output(/Invalid acronym/).to_stderr_from_any_process
   end
+
+  it "not warn when an acronym is valid" do
+    expect do
+      RelatonBipm::Committee.new acronym: "CCL-CCTF-WGFS"
+    end.not_to output.to_stderr_from_any_process
+  end
 end
