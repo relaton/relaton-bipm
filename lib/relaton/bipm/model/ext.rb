a5990917aaf78424e4d25bb86bcfa2f56425671f
Andrei Kislichenko <android.2net@gmail.com>
2026-04-16 13:06:25 -0400

    update lutaml-model to v0.8.0 and YAML fixtures for lutaml-model and relaton-bib compatibility

--- lib/relaton/bipm/model/ext.rb
+++ lib/relaton/bipm/model/ext.rb
@@ -10,23 +10,25 @@ module Relaton
         kg_h_c_deltanu kg_h m_c_deltanu m_c mol_NA s_deltanu
       ]
 
-      attribute :schema_version, method: :get_schema_version
       attribute :doctype, Doctype
+      attribute :structuredidentifier, StructuredIdentifier
       attribute :comment_period, CommentPeriod
       attribute :si_aspect, :string, values: SI_ASPECTS
       attribute :meeting_note, :string
-      attribute :structuredidentifier, StructuredIdentifier
 
       xml do
         map_element "comment-period", to: :comment_period
         map_element "si-aspect", to: :si_aspect
         map_element "meeting-note", to: :meeting_note
-        map_element "structuredidentifier", to: :structuredidentifier
       end
 
-      def get_schema_version
-       Relaton.schema_versions["relaton-model-bipm"]
+      key_value do
+        map_element "comment_period", to: :comment_period
+        map_element "si_aspect", to: :si_aspect
+        map_element "meeting_note", to: :meeting_note
       end
+
+      def get_schema_version = Relaton.schema_versions["relaton-model-bipm"]
     end
   end
 end
